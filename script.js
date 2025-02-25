8929b684afc454ae0329bb9356dbb3330a09e64d
MenukaSankalpa <menuka.sankalpa@gmail.com>
2025-02-25 09:58:10 +0530

    made function handlecommands in js

--- script.js
+++ script.js
@@ -3,8 +3,27 @@ recognition.Lang = "en-US"
 
 const btn = document.querySelector("#btn")
 btn.addEventListener("click", ()=>{
+
+    //convert text to voice
     function speak(text){
         const abc = new SpeechSynthesisUtterance(text)
         window.speechSynthesis.speak(abc);
     }
+
+    function handleCommands(command){
+        if(command.includes("Open youtube")){
+            speak("Opening Youtube... ")
+            window.open("https://www.youtube.com", "_blank")
+        }else if(command.includes("Open facebook")){
+            speak("Opening facebook... ")
+            window.open("https://www.facebook.com", "_blank")
+        }else if(command.includes("Open instagram")){
+            speak("Opening instagram... ")
+            window.open("https://www.instagram.com", "_blank")
+        }else{
+            speak("search on Youtube")
+        }
+    }
+    speak("Hello Tom, how can i help you")
 });
+
